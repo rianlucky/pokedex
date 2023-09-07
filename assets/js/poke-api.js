dc6f1afb064e886bf2e0e9c14a4e9747b83c6676
Rian <142761876+rianlucky@users.noreply.github.com>
2023-09-06 21:37:23 -0300

    Adding Project

---
+++ assets/js/poke-api.js
@@ -0,0 +1,35 @@
+const pokeApi = {}
+
+function converPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.id  = pokeDetail.id
+    pokemon.name  = pokeDetail.name
+
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.home.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(converPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset, limit) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
+
